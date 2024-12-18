b8159d015dd4a183ec2458756a39836f9bafd01f
SJF-ECNU <10225501403@stu.ecnu.edu.cn>
2024-12-18 12:00:46 +0800

    fix

--- app/actions.ts
+++ app/actions.ts
@@ -4,7 +4,7 @@ import { encodedRedirect } from "@/utils/utils";
 import { createClient } from "@/utils/supabase/server";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-
+export const runtime = 'edge';
 export const signUpAction = async (formData: FormData) => {
   const email = formData.get("email")?.toString();
   const password = formData.get("password")?.toString();
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
-
+export const runtime = 'edge';
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
